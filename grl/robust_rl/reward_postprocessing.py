7fdf7c5843a2b295036e203e6b22e45df8b29302
JB <johnblanier@gmail.com>
2021-01-11 23:27:33 -0800

    initial simple grid suite

--- grl/robust_rl/reward_postprocessing.py
+++ grl/robust_rl/reward_postprocessing.py
@@ -14,6 +14,15 @@ class TrajectoryRewardPostProcessing(ABC):
         raise NotImplementedError
 
 
+class NoOpPostProcessing(TrajectoryRewardPostProcessing):
+
+    def __call__(self,
+                 agent_id_for_trajectory: AgentID,
+                 trajectory_rewards: List[float],
+                 agent_total_rewards: Dict[AgentID, float]) -> List[float]:
+        return trajectory_rewards
+
+
 class StandardRobustRLPostProcessing(TrajectoryRewardPostProcessing):
 
     def __init__(self,
@@ -200,3 +209,68 @@ class ConstrainedRobustRLSingleRunPostProcessingRev2(TrajectoryRewardPostProcess
         else:
             raise NotImplementedError(f"Unknown agent id for reward postprocessing: {agent_id_for_trajectory}")
 
+
+class PAIREDSingleRunPostProcessing(TrajectoryRewardPostProcessing):
+
+    def __init__(self,
+                 adversary_agent_id: AgentID,
+                 protagonist_agent_id: AgentID,
+                 antagonist_agent_id: AgentID):
+
+        self.adversary_agent_id = adversary_agent_id
+        self.protagonist_agent_id = protagonist_agent_id
+        self.antagonist_agent_id = antagonist_agent_id
+
+    def __call__(self,
+                 agent_id_for_trajectory: AgentID,
+                 trajectory_rewards: List[float],
+                 agent_total_rewards: Dict[AgentID, float]) -> List[float]:
+
+        protagonist_single_episode_reward: float = agent_total_rewards[self.protagonist_agent_id]
+        antagonist_single_episode_reward: float = agent_total_rewards[self.antagonist_agent_id]
+
+        if agent_id_for_trajectory == self.protagonist_agent_id:
+            protag_new_trajectory_rewards = np.zeros_like(trajectory_rewards)
+            protag_new_trajectory_rewards[-1] = protagonist_single_episode_reward - antagonist_single_episode_reward
+            return list(protag_new_trajectory_rewards)
+        elif agent_id_for_trajectory == self.antagonist_agent_id:
+            antag_new_trajectory_rewards = np.zeros_like(trajectory_rewards)
+            antag_new_trajectory_rewards[-1] = antagonist_single_episode_reward - protagonist_single_episode_reward
+            return list(antag_new_trajectory_rewards)
+        elif agent_id_for_trajectory == self.adversary_agent_id:
+            adversary_new_trajectory_rewards = np.zeros_like(trajectory_rewards)
+            adversary_new_trajectory_rewards[-1] = antagonist_single_episode_reward - protagonist_single_episode_reward
+            return list(adversary_new_trajectory_rewards)
+        else:
+            raise NotImplementedError(f"Unknown agent id for reward postprocessing: {agent_id_for_trajectory}")
+
+
+class PAIREDSingleRunPostProcessingAdversaryOnly(TrajectoryRewardPostProcessing):
+
+    def __init__(self,
+                 adversary_agent_id: AgentID,
+                 protagonist_agent_id: AgentID,
+                 antagonist_agent_id: AgentID):
+
+        self.adversary_agent_id = adversary_agent_id
+        self.protagonist_agent_id = protagonist_agent_id
+        self.antagonist_agent_id = antagonist_agent_id
+
+    def __call__(self,
+                 agent_id_for_trajectory: AgentID,
+                 trajectory_rewards: List[float],
+                 agent_total_rewards: Dict[AgentID, float]) -> List[float]:
+
+        if agent_id_for_trajectory == self.protagonist_agent_id:
+            return trajectory_rewards
+        elif agent_id_for_trajectory == self.antagonist_agent_id:
+            return trajectory_rewards
+        elif agent_id_for_trajectory == self.adversary_agent_id:
+            protagonist_single_episode_reward: float = agent_total_rewards[self.protagonist_agent_id]
+            antagonist_single_episode_reward: float = agent_total_rewards[self.antagonist_agent_id]
+
+            adversary_new_trajectory_rewards = np.zeros_like(trajectory_rewards)
+            adversary_new_trajectory_rewards[-1] = antagonist_single_episode_reward - protagonist_single_episode_reward
+            return list(adversary_new_trajectory_rewards)
+        else:
+            raise NotImplementedError(f"Unknown agent id for reward postprocessing: {agent_id_for_trajectory}")
